fcfbef4f00a8076fe7a5e3101fec2c0d77259bb5
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-23 11:51:00 +0100

    PHP Render Approach

--- our-blocks/banner.js
+++ our-blocks/banner.js
@@ -29,21 +29,14 @@ function EditComponent() {
 
   return (
     <div className="page-banner">
-      <div className="page-banner__bg-image" style={{ backgroundImage: "url('/wp-content/themes/university-block-theme/images/library-hero.jpg')" }}></div>
+      <div className="page-banner__bg-image" style={{ backgroundImage: "url('/wp-content/themes/fictional-block-theme/images/library-hero.jpg')" }}></div>
       <div className="page-banner__content container t-center c-white">
-      <InnerBlocks allowedBlocks={["ourblocktheme/genericheading", "ourblocktheme/genericbutton"]} />
+        <InnerBlocks allowedBlocks={["ourblocktheme/genericheading", "ourblocktheme/genericbutton"]} />
       </div>
     </div>
   )
 }
 
 function SaveComponent() {
-  return (
-    <div className="page-banner">
-      <div className="page-banner__bg-image" style={{ backgroundImage: "url('/wp-content/themes/university-block-theme/images/library-hero.jpg')" }}></div>
-      <div className="page-banner__content container t-center c-white">
-        <InnerBlocks.Content />
-      </div>
-    </div>
-  )
+  return <InnerBlocks.Content />
 }
