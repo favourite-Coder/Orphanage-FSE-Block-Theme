3a2aa9929cb6f688fd08f9c007bf5d39e87d82c5
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-24 06:47:00 +0100

    Update singleprofessor.php

--- our-blocks/singleprofessor.php
+++ our-blocks/singleprofessor.php
@@ -53,7 +53,7 @@ while(have_posts()) {
 
             ?>
 
-            <span class="like-box" data-like="<?php echo $existQuery->posts[0]->ID; ?>" data-professor="<?php the_ID(); ?>" data-exists="<?php echo $existStatus; ?>">
+            <span class="like-box" data-like="<?php if (isset($existQuery->posts[0]->ID)) echo $existQuery->posts[0]->ID; ?>" data-professor="<?php the_ID(); ?>" data-exists="<?php echo $existStatus; ?>">
               <i class="fa fa-heart-o" aria-hidden="true"></i>
               <i class="fa fa-heart" aria-hidden="true"></i>
               <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
